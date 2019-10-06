ece1b588b7f8ee310439a2985bb806486216c05c
glebasty <Borzilololoh123>
2019-10-06 17:08:08 +0300

    Database creation change for production

--- app/db.ts
+++ app/db.ts
@@ -1,21 +1,31 @@
 const _ = require('lodash');
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    username: process.env.POSTGRES_USERNAME,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    isolationLevel: Sequelize.Transaction.ISOLATION_LEVELS.SERIALIZABLE,
-    dialect: 'postgres',
-    pool: {
-        max: 25,
-        min: 0,
-        idle: 20000,
-        acquire: 20000
-    }
-});
+let sequelize: any = null;
+
+if (process.env.DATABASE_URL) {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect:  'postgres',
+        logging:  true //false
+    });
+} else {
+    sequelize = new Sequelize({
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT,
+        username: process.env.POSTGRES_USERNAME,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DB,
+        isolationLevel: Sequelize.Transaction.ISOLATION_LEVELS.SERIALIZABLE,
+        dialect: 'postgres',
+        pool: {
+            max: 25,
+            min: 0,
+            idle: 20000,
+            acquire: 20000
+        }
+    });
+}
+
 
 const loadFile = (file: any) => sequelize.import(`../db/models/${file}.js`);
 
