6c16888c7fb5fa1c5f2daf48d4e78c600dc15b6e
glebasty <Borzilololoh123>
2020-01-04 14:13:30 +0200

    Removed old migrations

--- db/migrations/20190806143907-create-bet.js
+++ db/migrations/20190806143907-create-bet.js
@@ -7,7 +7,7 @@ module.exports = {
                 primaryKey: true,
                 type: Sequelize.INTEGER
             },
-            sportType: {
+            sportTypeId: {
                 type: Sequelize.INTEGER
             },
             competition: Sequelize.TEXT,
