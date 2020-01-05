1da7f813ed2be5fc3e59c4f43b20824e7d553295
glebasty <Borzilololoh123>
2020-01-06 00:28:07 +0200

    Added subscription fetching.

--- src/modules/subscription/controller.ts
+++ src/modules/subscription/controller.ts
@@ -17,7 +17,7 @@ class Controller {
             liqpay.api("request", {
                 "action"   : "status",
                 "version"  : "3",
-                "order_id" : '1'
+                "order_id" : 1211622524
             }, (json: any) => {
                 console.log('result', json);
             });
@@ -33,6 +33,39 @@ class Controller {
         }
     }
 
+
+    static async subscriptions(req: Request, res: Response) {
+
+        const subscriptions = [
+            { title: 'Subscription for day', value: 200, description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica' },
+            { title: 'Subscription for week', value: 400, description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica' },
+            { title: 'Subscription for month', value: 1000, description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica', }
+        ];
+        const liqpay = new LiqPay(process.env.LIQ_PAY_PUBLIC_KEY, process.env.LIQ_PAY_PRIVATE_KEY);
+
+        console.log('liqpay', liqpay);
+        const userSubscriptions = subscriptions.map((subscription: any) => {
+            const { data, signature } =  liqpay.cnb_object({
+                public_key: process.env.LIQ_PAY_PUBLIC_KEY,
+                version: 3,
+                order_id: 100,
+                description: subscription.description,
+                amount: subscription.value,
+                currency: 'RUB',
+                action: 'PAY'
+            });
+
+            console.log('data', data);
+            console.log('signature', signature);
+            return {
+                ...subscription,
+                data,
+                signature
+            }
+
+        });
+        res.status(200).send(userSubscriptions);
+    }
 }
 
 export default Controller;
--- src/modules/subscription/routes.ts
+++ src/modules/subscription/routes.ts
@@ -7,5 +7,7 @@ const router = Router();
 // subscription
 router.post("/payment",  SubscriptionController.payment);
 
+router.get("/subscriptions", SubscriptionController.subscriptions)
+
 
 export default router;
