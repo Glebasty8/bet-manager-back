9b0c292916056b5965b746bbcce83dd7a8b7ea38
glebasty <Borzilololoh123>
2020-02-10 13:49:16 +0200

    Setup sending of notification of bet in telegram.

--- db/seeders/20190819221211-subscriptions.js
+++ db/seeders/20190819221211-subscriptions.js
@@ -4,25 +4,25 @@ module.exports = {
     up: (queryInterface, Sequelize) => {
         return queryInterface.bulkInsert('subscriptions', [
             {
-                title: 'Subscription for day',
+                title: 'Подписка "День"',
                 cost: 200,
-                description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+                description: 'Как минимум один матч на сегодня или завтра будет в вашем распоряжении!',
                 subscriptionTimeInHours: 24,
                 createdAt: new Date(),
                 updatedAt: new Date(),
             },
             {
-                title: 'Subscription for week',
-                cost: 400,
-                description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+                title: 'Подписка "Неделя"',
+                cost: 700,
+                description: 'Так же, как и в прошлой, один день - минимум, и как правило, один прогноз. Очень удобный вариант, чтобы затронуть и еврокубковые, и внутренние чемпионаты',
                 subscriptionTimeInHours: 168,
                 createdAt: new Date(),
                 updatedAt: new Date(),
             },
             {
-                title: 'Subscription for month',
-                cost: 1000,
-                description: 'Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica',
+                title: 'Подписка "Месяц"',
+                cost: 2000,
+                description: 'От старта - до самого финиша. Период в 30 дней - это наш главный ориентир выхода на положительный баланс. Цель - с первого по последнее число каждого месяца должен быть получен плюсовой баланс. С каждого нового месяца баланс обнуляется.',
                 subscriptionTimeInHours: 720,
                 createdAt: new Date(),
                 updatedAt: new Date(),
--- src/modules/bet/controller.ts
+++ src/modules/bet/controller.ts
@@ -1,15 +1,33 @@
 import { Request, Response} from "express";
+const {
+    Telegram
+} = require('telegraf');
+const moment = require('moment');
+const _ = require('lodash');
 import { models } from '../../db';
+
+const tg = new Telegram(process.env.BOT_TOKEN)
 
 class Controller {
 
     static async getAll(req: Request, res: Response) {
         const bets = await models.Bet.findAll();
+        console.log('bets', bets);
         return res.status(200).send(bets);
     }
 
     static async create(req: Request, res: Response) {
         const bet = await models.Bet.create(req.body, { returning: true });
+        const { eventDate, sportTypeId, competition, forecast, coefficient, betAmount, competitors, isFree } = bet;
+        if (isFree) {
+            const eventDateFormatted = moment(eventDate).tz('Europe/Moscow').format('DD-MM-YYYY HH:mm');
+            const sportType = await models.SportType.findByPk(sportTypeId);
+            const sportTypeName = _.get(sportType, 'name', '');
+            const competitorsFormatted = competitors.reduce((acc: any, curr: any) => {
+                return `${acc} - ${curr}`;
+            }, '');
+            tg.sendMessage(process.env.PUBLIC_GROUP_ID, `Начало матча: ${eventDateFormatted} (МСК)\n ${sportTypeName}.${competition} \n ${competitorsFormatted}: ${forecast} - ${coefficient}\n Рекомендуемая сумма ставки: ${betAmount}\n Удачи! 🍀`);
+        }
         return res.status(201).send(bet);
     }
 
